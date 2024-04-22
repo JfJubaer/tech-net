db2d2204087f6ca1145fa197dd34d81f4d1f7be1
JfJubaer <skk5727@gmail.com>
2024-04-22 21:20:31 +0600

    first commit

---
+++ src/pages/Login.tsx
@@ -0,0 +1,7 @@
+export default function Login() {
+  return (
+    <>
+      <div className="container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">login</div>
+    </>
+  );
+}
